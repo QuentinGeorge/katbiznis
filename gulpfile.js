c84b44fece132bafc4d1e8a56238cf0772bce5e2
QuentinGeorge <quentingeorge7@gmail.com>
2017-02-09 15:42:32 +0100

    Setup gul env

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+/* QuentinGeorge/katbiznis
+/
+/  /gulpfile.js - Gulp tasks
+/  coded by Quentin
+/  started at 09/02/2017
+*/
+
+var gulp = require( "gulp" );
+
+// --- Task for images
+
+// --- Task for styles
+
+// --- Task for pug
+
+// --- Task for js
+
+// --- Watch tasks
+
+// --- Aliases
